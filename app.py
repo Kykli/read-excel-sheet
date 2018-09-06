0a40516cb4a5bbb6cdff60421ebe57508c6e6909
Kykli <luukkanen.eetu@gmail.com>
2018-09-06 20:50:37 +0300

    Improvements to user experience

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ class bcolors:
     red = "\033[0;31m"
     end = "\033[0m"
 
+replaceList = ["{", "}"]
+
 try:
     while True:
         print("******************")
@@ -19,26 +21,38 @@ try:
         number = input("")
 
         if number == "1":
-            while True:
+            restart = True
+            while restart:
                 try:
                     city = input("Provide city: ").title()
-                    print("\n" + str(FILENAME.allData[city]) + "\n")
-                    input("Press Enter to continue...")
+                    pickedCity = str(FILENAME.allData[city])
+                    print("\n" + re.sub("|".join(replaceList)," ", pickedCity.replace(",", "\n")) + "\n")    
+                    input("Press Enter to continue...\n")
                     break
-            except KeyError:
-                print(bcolors.red + "City not found!" + bcolors.end)
-                input("Press Enter to continue...")
+                except KeyError:
+                    print(bcolors.red + "City not found!" + bcolors.end)
+                    back = input("Press Enter to continue or 'b' to start over...\n")
+                    
+                    if back == "b":
+                        restart = False
+                        break
         
         elif number == "2":
-            while True:
+            restart = True
+            while restart:
                 try:
                     streetAddress = input("Provide street address: ").title()
-                    print("\n" + str(FILENAME.allData[streetAddress]) + "\n")
-                    input("Press Enter to continue...")
+                    pickedStreetAddress = str(FILENAME.allData[streetAddress])
+                    print("\n" + re.sub("|".join(replaceList)," ", pickedStreetAddress.replace(",", "\n")) + "\n")
+                    input("Press Enter to continue...\n")
                     break
-            except KeyError:
-                print(bcolors.red + "Street address not found!" + bcolors.end)
-                input("Press Enter to continue...")
+                except KeyError:
+                    print(bcolors.red + "Street address not found!" + bcolors.end)
+                    back = input("Press Enter to continue or 'b' to start over...\n")
+                    
+                    if back == "b":
+                        restart = False
+                        break
         else:
             print(bcolors.red + "Please choose 1 or 2." + bcolors.end)
             input("Press Enter to continue...")
